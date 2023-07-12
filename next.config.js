763424f4c456996cbb192f896f886fb37bd400d3
hitartha-gogoi <arnabgogoi83@gmail.com>
2023-07-12 17:44:51 +0530

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} **/
+
 const nextConfig = {
-  images: ["links.papareact.com"]
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
